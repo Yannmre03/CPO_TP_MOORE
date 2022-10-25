514c0ade882a84ecb4b9dc28e8b570e380daa00e
yann <yann.moore2004@gmail.com>
2022-10-25 17:13:41 +0200

    fin de la séance de TD

--- TP2_convertisseurObjet_MOORE/src/tp2_convertisseurobjet_moore/Convertisseur.java
+++ TP2_convertisseurObjet_MOORE/src/tp2_convertisseurobjet_moore/Convertisseur.java
@@ -9,10 +9,23 @@ package tp2_convertisseurobjet_moore;
  * @author Administrateur
  */
 public class Convertisseur {
-    int nbConversions;  
+    int nbConversions;                  // nombre de conversions a effectuer 
+
+    @Override
+    public String toString () {
+    return "nb de conversions"+ nbConversions;
+}
+    public Convertisseur () {       //constructeur 
+    nbConversions = 0 ;
+}
     
-    public double CelciusVersKelvin(double tempCelcius) {
+    public double CelciusVersKelvin(double tempCelcius) {       // converti des celcius en kelvin (float -> float) 
+        nbConversions +=1;
         return tempCelcius + 273.15;
+    }
+    public double KelvinVersCelcius(double tempKelvin) {
+        nbConversions +=1;
+        return tempKelvin - 273.15;
     }
     
 }
