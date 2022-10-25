5c8aa6fc7c61a93d865bd670ddc346fda68727ca
yann <yann.moore2004@gmail.com>
2022-10-25 17:01:14 +0200

    début de la partie 2 du tp / création du projet

---
+++ TP2_convertisseurObjet_MOORE/src/tp2_convertisseurobjet_moore/Convertisseur.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp2_convertisseurobjet_moore;
+
+/**
+ *
+ * @author Administrateur
+ */
+public class Convertisseur {
+    int nbConversions;  
+    
+    public double CelciusVersKelvin(double tempCelcius) {
+        return tempCelcius + 273.15;
+    }
+    
+}
