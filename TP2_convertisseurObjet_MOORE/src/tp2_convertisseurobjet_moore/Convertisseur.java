3cb545815c16d9974b951414187d15f7fc031a1f
yann <yann.moore2004@gmail.com>
2022-10-26 11:06:33 +0200

    fin partie 2 + commentaires

--- TP2_convertisseurObjet_MOORE/src/tp2_convertisseurobjet_moore/Convertisseur.java
+++ TP2_convertisseurObjet_MOORE/src/tp2_convertisseurobjet_moore/Convertisseur.java
@@ -15,31 +15,31 @@ public class Convertisseur {
     nbConversions = 0 ;
     }
     public double CelciusVersKelvin(double tempCelcius) {       // converti des celcius en kelvin (float -> float) 
-        nbConversions +=1;
-        return tempCelcius + 273.15;
+        nbConversions +=1;                                      // met à jour le nb de conversions
+        return tempCelcius + 273.15;                            // retourne la température convertie 
     }
-    public double KelvinVersCelcius(double tempKelvin) {
+    public double KelvinVersCelcius(double tempKelvin) {        // converti des Kelvin en Celcius (float -> float)  
         nbConversions +=1;
         return tempKelvin - 273.15;
     }
-    public double FarenheitVersCelcius(double tempFarenheit) {
+    public double FarenheitVersCelcius(double tempFarenheit) {  // etc 
         nbConversions +=1;
         return (tempFarenheit - 32.0)/1.8;
     }
-    public double CelciusVersFarenheit(double tempCelcius) {
+    public double CelciusVersFarenheit(double tempCelcius) {        
         nbConversions +=1;
         return (tempCelcius *1.8 + 32);
     }
     public double FarenheitVersKelvin(double tempFarenheit) {
         nbConversions += 1; 
-        return (CelciusVersKelvin(FarenheitVersCelcius(tempFarenheit)));
+        return (tempFarenheit-32)/1.8 + 273.15;
     }
     public double KelvinVersFarenheit(double tempKelvin) {
         nbConversions += 1; 
-        return CelciusVersFarenheit(KelvinVersCelcius(tempKelvin));
+        return (tempKelvin-273.15)*1.8 + 32;
     }
     @Override
     public String toString () {
-    return "nb de conversions"+ nbConversions;
+    return "nb de conversions: "+ nbConversions;
     }
 }
