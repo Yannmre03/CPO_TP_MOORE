607b64509ebf76c5c6d2521ccd63acd57a24de58
yann <yann.moore2004@gmail.com>
2022-10-28 15:35:11 +0200

    question 10 finie et commentée

--- TP2_relation_1_MOORE/src/tp2_relation_1_moore/Personne.java
+++ TP2_relation_1_MOORE/src/tp2_relation_1_moore/Personne.java
@@ -26,15 +26,22 @@ public class Personne {
     }
     
     public boolean ajouter_voiture( Voiture voiture_a_ajouter) {
-        if (voiture_a_ajouter.Proprietaire == null) {
+        if (voiture_a_ajouter.Proprietaire != null) {           // passe à l'étape suivante seulement si la voiture n'a pas de propriétaire
             return false; 
             }
-        for (int i = 0; i <= 3; i++ ) {
-            
-            return true;
-            
+        for (int i = 0; i < 3; i++ ) {      // parcours la liste de voiture de la personne
+            if (this.liste_voitures[i] == null) {           // passe à la suite seulement si une place du tableau est vide
+                this.liste_voitures[this.nbVoitures] = voiture_a_ajouter ;       //assigne une voiture à la personne désignée
+                this.nbVoitures += 1 ;          // incrémente son nombrede voiture                
+                voiture_a_ajouter.Proprietaire = this ;     // ajoute le proprio à la voiture
+                return true;    
+            }
+            else { if (i == 2){             // return false si tout le tableau a été parcouru mais qu'il n'y a plus de place
+               return false; 
+            }
+                
+            }          
         }
-        
         return false;
     }
 
