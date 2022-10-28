2de353a4bc1416c5235aec7a75ecefdb34a6d29f
yann <yann.moore2004@gmail.com>
2022-10-28 15:21:56 +0200

    question 10 en cours

--- TP2_relation_1_MOORE/src/tp2_relation_1_moore/TP2_relation_1_MOORE.java
+++ TP2_relation_1_MOORE/src/tp2_relation_1_moore/TP2_relation_1_MOORE.java
@@ -15,14 +15,37 @@ public class TP2_relation_1_MOORE {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        Voiture uneClio = new Voiture ("Clio", "Renault", 5 ) ;
+        Voiture uneClio = new Voiture ("Clio", "Renault", 5 ) ;     //ajout manuel de voitures
         Voiture uneAutreClio = new Voiture ("Clio", "Renault", 5 ) ;
         Voiture une2008 = new Voiture ("2008", "Peugeot", 6 ) ;
         Voiture uneMicra = new Voiture ("Micra", "Nissan", 4 ) ;
-        Personne bob = new Personne("Bobby", "Sixkiller");
-        Personne reno = new Personne("Reno", "Raines");
-        System.out.println("liste des voitures disponibles: \n "+ uneClio +
-        "\n" + uneAutreClio + "\n" + une2008 + "\n" + uneMicra ) ;
+        Voiture une206 = new Voiture ("206", "Peugeot", 4 ) ;
+        
+        Personne Bob = new Personne("Bobby", "Sixkiller");      // ajout manuel de personnes
+        Personne Reno = new Personne("Reno", "Raines");
+        
+        //System.out.println("liste des voitures disponibles: \n "+ uneClio +
+        //"\n" + uneAutreClio + "\n" + une2008 + "\n" + uneMicra ) ;      // affiche les voitures disponibles
+        
+        Bob.liste_voitures[Bob.nbVoitures] = uneClio ;       //assigne une voiture à Bob
+        Bob.nbVoitures += 1 ;                    
+        uneClio.Proprietaire = Bob ;        // assigne le propriétaire Bob à uneClio
+        Bob.liste_voitures[Bob.nbVoitures] = une2008;    //seconde voiture
+        Bob.nbVoitures += 1 ;                    
+        une2008.Proprietaire = Bob ;
+        
+        Reno.liste_voitures[Reno.nbVoitures] = uneMicra ;       //assigne une voiture à Bob
+        Reno.nbVoitures += 1 ;                    
+        uneClio.Proprietaire = Reno ;        // assigne le propriétaire Reno à uneMicra
+        Reno.liste_voitures[Reno.nbVoitures] = uneAutreClio;    //seconde voiture
+        Reno.nbVoitures += 1 ;                    
+        une2008.Proprietaire = Reno ;
+        
+        //System.out.println("la premiere voiture de Bob est " +
+        //Bob.liste_voitures[0]) ;       //tester le fonctionnement 
+        //System.out.println( " \n\npropriétaire de la 2008: " + une2008.Proprietaire.nom + "\n");
+        //This = Bob
+        Bob.ajouter_voiture(uneClio);
         }
     
 }
