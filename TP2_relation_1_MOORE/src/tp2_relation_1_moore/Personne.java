7121eaec062746eb3990088897b39926a69b40fa
yann <yann.moore2004@gmail.com>
2022-10-28 15:22:06 +0200

    question 10 en cours

--- TP2_relation_1_MOORE/src/tp2_relation_1_moore/Personne.java
+++ TP2_relation_1_MOORE/src/tp2_relation_1_moore/Personne.java
@@ -13,19 +13,29 @@ public class Personne {
     int nbVoitures;
     Voiture [] liste_voitures;
     
-    public Personne (String unPrenom, String unNom) {
+    public Personne (String unPrenom, String unNom) {       // constructeur
     nom = unNom;
     prenom = unPrenom;
+    liste_voitures = new Voiture [3] ;
+    nbVoitures = 0;
     }
     
     @Override
-    public String toString () {
-        return "personne:" + nom + " " + prenom;
+    public String toString () {                             // méthode string 
+        return prenom + " " + nom + " a " + nbVoitures + " voiture(s). Voici ses voitures: " + liste_voitures[0] + liste_voitures[1] + liste_voitures[2] ;
     }
     
-    public String Personne(String unPrenom, String unNom) {
-        nom = unNom;
-        prenom = unPrenom;
-        return prenom + nom;
+    public boolean ajouter_voiture( Voiture voiture_a_ajouter) {
+        if (voiture_a_ajouter.Proprietaire == null) {
+            return false; 
+            }
+        for (int i = 0; i <= 3; i++ ) {
+            
+            return true;
+            
+        }
+        
+        return false;
     }
+
 }
