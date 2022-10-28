3cf71d633e904ee9ee78be0c1e1a79d5c1890260
yann <yann.moore2004@gmail.com>
2022-10-28 16:15:29 +0200

    question 5 finie

--- TP2_manip_MOORE/src/tp2_manip_moore/TP2_manip_MOORE.java
+++ TP2_manip_MOORE/src/tp2_manip_moore/TP2_manip_MOORE.java
@@ -18,14 +18,24 @@ public class TP2_manip_MOORE {
         Tartiflette assiette1 = new Tartiflette(500) ;
         Tartiflette assiette2 = new Tartiflette(600) ;
         Tartiflette assiette3 = assiette2 ; 
-        //nous avonsréellement que 2 tartiflettes, assiette3 et assiette2 ont la même référence objet
+        //nous avons réellement que 2 tartiflettes, assiette3 et assiette2 sont deux références qui pointent vers le même objet
         System.out.println("nb de calories de Assiette 1 : " +
         assiette1.nbCalories) ;
         System.out.println("nb de calories de Assiette 2 : " +
         assiette2.nbCalories) ;
         System.out.println("nb de calories de Assiette 3 : " +
         assiette3.nbCalories) ;
-        // QUESTION PROF 
+        assiette3.modifier_calories(550); 
+        System.out.println("nb de calories de Assiette 2 après modification de l'assiette3 : " +
+        assiette2.nbCalories) ;
+        System.out.println("nb de calories de Assiette 3 après sa modification : " +
+        assiette3.nbCalories) ;
+        //on remarque que le nombre de calories de l'assiette2 change lorsque l'on modifie l'assiette3: c'est donc le même objet
+        assiette2 = assiette1;
+        assiette1 = assiette3; //nous avons bien interverti 1 et 2 car 3 et 2 référencent le même objet
+        //Moussaka assiette666 = assiette1 ;
+        //Moussaka assiette667 = new Tartiflette(500) ;
+        
         
     }
     
