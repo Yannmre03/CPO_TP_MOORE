a374cbaf0759f4d975b9aedbeea8cf1bd135c2b0
yann <yann.moore2004@gmail.com>
2022-11-15 16:30:48 +0100

    début TP init

---
+++ TP3_MOORE/src/tp3_moore/TP3_MOORE.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp3_moore;
+
+/**
+ *
+ * @author Administrateur
+ */
+public class TP3_MOORE {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        bâton bat = new bâton ("bat", 20);
+        System.out.println(bat);
+    }
+    
+}
