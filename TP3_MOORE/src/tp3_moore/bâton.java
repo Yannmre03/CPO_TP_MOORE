9d5fc2b5bd36546eea3467917adc12f3b7c00b6e
yann <yann.moore2004@gmail.com>
2022-11-15 16:31:10 +0100

    début TP init

---
+++ TP3_MOORE/src/tp3_moore/bâton.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp3_moore;
+
+/**
+ *
+ * @author Administrateur
+ */
+public class bâton extends Arme {
+    public bâton(String unNom, int nivAttaque) {
+        nom = unNom;
+        niveauAttaque = nivAttaque ;
+    }
+}
