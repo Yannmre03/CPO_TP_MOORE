518e52f89dc6acd21a7630baac2321db21b05e3a
yann <yann.moore2004@gmail.com>
2022-11-15 16:31:16 +0100

    début TP init

---
+++ TP3_MOORE/src/tp3_moore/Epee.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp3_moore;
+
+/**
+ *
+ * @author Administrateur
+ */
+public class Epee extends Arme{
+    public Epee(String unNom, int nivAttaque) {
+        nom = unNom;
+        niveauAttaque = nivAttaque ;
+    }
+}
